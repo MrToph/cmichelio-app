6e56666ec7163caf9af61f1d8a3e36a79551166a
Christoph Michel <christoph.michel.1992@gmail.com>
2016-11-04 16:54:03 +0100

    uses dynamic port

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import express from 'express'
+// Heroku dynamically assigns your app a port, so you can't set the port to a fixed number. Heroku adds the port to the env, so you can pull it from there. 
+const port = process.env.PORT || 5000
 var app = express()
 
 app.get('/', function (req, res) {
   res.send('Hello World! 123456')
 })
 
-app.listen(5000, function () {
-  console.log('Example app listening on port 5000!')
+app.listen(port, function () {
+  console.log(`Example app listening on port ${port}!`)
 })
