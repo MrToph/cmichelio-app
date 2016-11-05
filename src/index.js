f9aa9a1fab59f44d5c5c136caddbd3499ebbee76
Christoph Michel <christoph.michel.1992@gmail.com>
2016-11-05 16:38:33 +0100

    Access-Control-Allow-Origin

--- src/index.js
+++ src/index.js
@@ -5,6 +5,12 @@ import { getLatestTweet } from './services/twitter'
 const port = process.env.PORT || 5000
 var app = express()
 
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  next()
+})
+
 app.get('/', function (req, res) {
   // res.send(`Hello World! Started at ${now.toISOString()}`)
   getLatestTweet((tweet) => {
